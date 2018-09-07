941e2f4f0b6612a911dc897e967f6aac8d91c4bb
Akim Desire <adesi006.ng@gmail.com>
2018-09-07 11:38:09 -0400

    backend functional

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+const cats = [
+  {
+    imageURL:
+      "https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg",
+    imageDescription:
+      "Orange bengal cat with black stripes lounging on concrete.",
+    name: "Fluffy",
+    sex: "Female",
+    age: 2,
+    breed: "Bengal",
+    story: "Thrown on the street"
+  }
+];
+const dogs = [
+  {
+    imageURL:
+      "http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg",
+    imageDescription:
+      "A smiling golden-brown golden retreiver listening to music.",
+    name: "Zeus",
+    sex: "Male",
+    age: 3,
+    breed: "Golden Retriever",
+    story: "Owner Passed away"
+  }
+];
 
-const { PORT, CLIENT_ORIGIN } = require('./config');
-const { dbConnect } = require('./db-mongoose');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const { PORT, CLIENT_ORIGIN } = require("./config");
+const { dbConnect } = require("./db-mongoose");
 // const {dbConnect} = require('./db-knex');
 
 const app = express();
 
 app.use(
-  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-    skip: (req, res) => process.env.NODE_ENV === 'test'
+  morgan(process.env.NODE_ENV === "production" ? "common" : "dev", {
+    skip: (req, res) => process.env.NODE_ENV === "test"
   })
 );
 
@@ -22,13 +49,33 @@ app.use(
   })
 );
 
+app.get("/api/cat", (req, res, next) => {
+  res.json(cats[0]);
+});
+
+app.delete("/api/cat", (req, res, next) => {
+  cats.shift();
+  console.log(cats);
+  res.sendStatus(204);
+});
+
+app.get("/api/dog", (req, res, next) => {
+  res.json(dogs[0]);
+});
+
+app.delete("/api/dog", (req, res, next) => {
+  dogs.shift();
+  console.log(dogs);
+  res.sendStatus(204);
+});
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
       console.info(`App listening on port ${server.address().port}`);
     })
-    .on('error', err => {
-      console.error('Express failed to start');
+    .on("error", err => {
+      console.error("Express failed to start");
       console.error(err);
     });
 }
